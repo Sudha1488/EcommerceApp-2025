9e409d0ffb23d172d5c343c262ee31cd171ecf98
Sudhanand Tiwari <sudhanand.1488@gmail.com>
2025-04-15 22:17:37 +0530

    Updated auth controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -88,7 +88,7 @@ export const login = async (req, res) => {
         role: user.role,
       });
     } else {
-      res.status(401).json({ message: "Invalid credentials" });
+      res.status(400).json({ message: "Invalid credentials" });
     }
   } catch (error) {
     res.status(500).json({ message: "Something went wrong while logging in" });
