5d857d102f06909b31e961a9ada62c29f17caf98
Sudhanand Tiwari <sudhanand.1488@gmail.com>
2025-04-15 13:26:45 +0530

    Last attempt fixing 1

--- backend/server.js
+++ backend/server.js
@@ -48,10 +48,15 @@ app.use("/api/analytics", analyticRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
+
+console.log("ENV:", process.env.NODE_ENV);
+console.log("CLIENT_URL:", process.env.CLIENT_URL);
+console.log("Static path:", path.join(__dirname, "/frontend/dist"));
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   connectDB();
